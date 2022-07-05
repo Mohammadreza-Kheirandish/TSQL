208ad67c709f4f7694dde9ee6d3fca3d21d3c4a8
Mohammadreza Kheirandish <74610048+Mohammadreza-Kheirandish@users.noreply.github.com>
2022-07-06 00:32:55 +0430

    Create Joins.sql

---
+++ Joins.sql
@@ -0,0 +1,32 @@
+//Inner join
+
+SELECT
+  * 
+FROM 
+  dbo.Employee As Employee
+INNER JOIN 
+  dbo.city As City
+ON
+  Employee.CityID = City.ID 
+  
+  
+//Inner Join with Group By and Having
+
+SELECT 
+  II.PartID AS Part ID,
+  SUM(II.Price) AS Price Agg
+ From 
+  SLS.Invoice AS I 
+INNER JOIN
+  SLS.InvoiceItem AS II
+ON 
+  I.InvoiceID = II.InvoiceID 
+WHERE
+  YEAR(I.DATE ) = 2014
+GROUP BY
+  II.PartID
+HAVING
+  SUM(II.Price) < 1000
+  
+//
+
